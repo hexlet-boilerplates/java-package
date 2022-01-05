c29b09e6924888f36c4623f0f87e3815fc75619a
Kirill Mokevnin <mokevnin@gmail.com>
2022-01-05 10:35:08 -0500

    add user
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

---
+++ src/main/java/io/hexlet/boilerplate/User.java
@@ -0,0 +1,19 @@
+package io.hexlet.boilerplate;
+
+class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
