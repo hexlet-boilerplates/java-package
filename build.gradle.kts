c949f2dd29714904673fe7651ebabde6b2928f90
Kirill Mokevnin <mokevnin@gmail.com>
2023-08-15 19:42:34 -0400

    fix indentation
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-  application
-  id("checkstyle")
-  id("io.freefair.lombok") version "8.1.0"
-  id("com.github.ben-manes.versions") version "0.47.0"
+    application
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.1.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 group = "io.hexlet"
@@ -14,22 +14,22 @@ version = "1.0-SNAPSHOT"
 application { mainClass.set("io.hexlet.blog.Application") }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  testImplementation(platform("org.junit:junit-bom:5.9.1"))
-  testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-  useJUnitPlatform()
-  // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
-  testLogging {
-    exceptionFormat = TestExceptionFormat.FULL
-    events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-    // showStackTraces = true
-    // showCauses = true
-    showStandardStreams = true
-  }
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
