e196611e6586d283c8056dd354a3bf2d641a31e5
Maksim Litvinov <maksim.litvinov@hexlet.io>
2023-12-26 09:48:44 +0300

    romove duplicate deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,6 @@ repositories { mavenCentral() }
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
