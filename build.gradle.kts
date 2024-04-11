87a505e5c49970b8c730b9b5aa0b456af31e963d
Maksim Litvinov <litvinovmksm@gamil.com>
2024-04-11 14:47:07 +0300

    update to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     application
     jacoco
     id("checkstyle")
-    id("io.freefair.lombok") version "8.4"
+    id("io.freefair.lombok") version "8.6"
     id("com.github.ben-manes.versions") version "0.50.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
