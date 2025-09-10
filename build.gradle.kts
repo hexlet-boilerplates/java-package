a61eaaaeade2219c5e9017253c698d5ab00d520a
Kirill Mokevnin <mokevnin@gmail.com>
2025-09-09 23:19:06 -0400

    update deps
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
+    // id("com.github.ben-manes.versions") version "0.52.0"
     id("com.diffplug.spotless") version "7.2.1"
 
     application
